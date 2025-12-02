1c5d5f334e3deba0fe6868d7c9714f93741e378e
Enma <c.enma69@gmail.com>
2025-12-02 07:15:48 -0500

    new

--- Zoologico.API/Program.cs
+++ Zoologico.API/Program.cs
@@ -35,7 +35,7 @@ namespace Zoologico.API
                 var app = builder.Build();
 
                 // Configure the HTTP request pipeline.
-                if (app.Environment.IsDevelopment())
+               // if (app.Environment.IsDevelopment())
                 {
                     app.UseSwagger();
                     app.UseSwaggerUI();
